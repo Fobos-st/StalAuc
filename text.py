29942acd8a418b2455768bcae297a2948a7b5a30
Amarsana <fipikmen3@gmail.com>
2023-09-13 13:13:12 +0800

    Создал функцию для создания текста текущего запроса пользователя

--- text.py
+++ text.py
@@ -1,3 +1,7 @@
+from DataBase.sqlite import get_request_user
+from DataBase.dbitem import search_item_name_by_id
+
+
 text_auc_lot = """
 Количество: {}
 Ставка: {}
@@ -13,3 +17,19 @@ notification_text = """
 
 QUALITY = ("Обычный", "Необычный", "Особый", "Редкий", "Исключительный")
 
+
+def current_request(user_id: int) -> str:
+    data = get_request_user(user_id)
+    if data[1] == 'None':
+        return "У вас нету запросов"
+    text = f"""
+Текущий запрос:
+предмет: {search_item_name_by_id(data[1])}
+цена: {data[2]} \n"""
+    if data[3] != 'None':
+        text += f"Качество: {QUALITY[int(data[3])]} \n"
+    if data[4] == "All":
+        text += "Уровень заточки: Любой"
+    else:
+        text += f"Уровень заточки: {data[4]}"
+    return text
