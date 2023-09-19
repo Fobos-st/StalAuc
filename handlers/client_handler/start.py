aea1117d115729010e9e467ed38d465d73830c24
Amarsana <fipikmen3@gmail.com>
2023-09-19 12:25:31 +0800

    change: исправил ошибку в тексте

--- handlers/client_handler/start.py
+++ handlers/client_handler/start.py
@@ -27,7 +27,7 @@ async def cmd_server_selection(callback_query: types.CallbackQuery):
 # @dp.callback_query_handler(text=['RU'])
 async def cmd_main(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, """
-В данном боте есть 2 функции
+В данном боте есть 3 функции
 1)Самая обычная проверка цен на аукционе
 2)Ожидание предмета на аукционе с определённой ценной и качеством в случае с артефактом
 (Получается при появление нужного предмета, с нужной, вам отправится сообщение об этом)
