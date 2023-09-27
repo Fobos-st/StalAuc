692c2fbd2f57926fa9c78deb9dfd62549ee94fdb
Amarsana <fipikmen3@gmail.com>
2023-09-27 15:16:04 +0800

    rework: Создал тайминги для создания графика

--- handlers/client_handler/price_history_chart.py
+++ handlers/client_handler/price_history_chart.py
@@ -3,8 +3,8 @@ import json
 import os
 from datetime import datetime, timedelta
 
-import mplfinance as mpf
-import pandas as pd
+# import mplfinance as mpf
+# import pandas as pd
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -29,6 +29,7 @@ from ..keyboard import cancel_inline_keyboard
 class CreateChart(StatesGroup):
     item_id = State()
     days = State()
+    timing = State()
 
 
 async def check_time(time: str, days) -> bool:
@@ -55,37 +56,48 @@ async def get_data_item(item_id):
 
 
 async def get_data_item_more_200(item_id, iteration):
-    PARAMS_CHECKED = {"limit": "200", "sort": "buyout_price", "additional": "true",
+    PARAMS_CHECK_MORE_200 = {"limit": "200", "sort": "buyout_price", "additional": "true",
                       "offset": f"{str(iteration * 200)}"}
-    data_item = await make_http_get_request(URL_GET_HISTORY_AUC_LOTS.format(item_id), HEADERS_1, PARAMS_CHECKED)
+    data_item = await make_http_get_request(URL_GET_HISTORY_AUC_LOTS.format(item_id), HEADERS_1, PARAMS_CHECK_MORE_200)
     data_item = json.loads(data_item)
     try:
         data_item = data_item['prices']
-        print(data_item, 59)
         return data_item
     except KeyError:
         ex = True
         await asyncio.sleep(10)
         while ex:
-            PARAMS_CHECKED = {"limit": "200", "sort": "buyout_price", "additional": "true",
-                              "offset": f"{str(iteration * 200)}"}
-            data_item = await make_http_get_request(URL_GET_HISTORY_AUC_LOTS.format(item_id), HEADERS_1, PARAMS_CHECKED)
+            data_item = await make_http_get_request(URL_GET_HISTORY_AUC_LOTS.format(item_id), HEADERS_1, PARAMS_CHECK_MORE_200)
             data_item = json.loads(data_item)
             try:
                 data_item = data_item['prices']
-                print(data_item, 59)
                 return data_item
             except KeyError:
                 await asyncio.sleep(10)
 
 
-async def parse_json_file(days, item_id):
+async def check_time_passed(start_time, end_time, minutes):
+    # Преобразуем строки с временем в объекты datetime
+    start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
+    end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%SZ")
+
+    # Разница во времени между start_time и end_time в минутах
+    time_diff = (start_time - end_time).total_seconds() / 60
+    # Проверяем, если разница больше или равна заданному количеству минут
+    if time_diff >= minutes:
+        return True
+    else:
+        return False
+
+
+async def parse_json_file_test(days, item_id, minutes):
     data_item = await get_data_item(item_id)
     iteration = 0
     counter = 0
     day = 0
+    count = 0
 
-    dates_sold = [str(data_item[counter]["time"])[:10]]
+    dates_sold = [data_item[counter]["time"]]
     open_prices = [data_item[counter]["price"] / data_item[counter]["amount"]]
     high_prices = [data_item[counter]["price"] / data_item[counter]["amount"]]
     low_prices = [data_item[counter]["price"] / data_item[counter]["amount"]]
@@ -99,79 +111,77 @@ async def parse_json_file(days, item_id):
             data_item = await get_data_item_more_200(item_id, iteration)
             counter = 0
         if await check_time(str(data_item[counter]["time"]), days):
-            if dates_sold[day] != str(data_item[counter]["time"])[:10]:
-                counter += 1
-                day += 1
-                dates_sold.append(str(data_item[counter]["time"])[:10])
+            if await check_time_passed(dates_sold[count], data_item[counter]['time'], minutes):
+                dates_sold.append(str(data_item[counter]["time"]))
                 open_prices.append(data_item[counter]["price"] / data_item[counter]["amount"])
                 high_prices.append(data_item[counter]["price"] / data_item[counter]["amount"])
                 low_prices.append(data_item[counter]["price"] / data_item[counter]["amount"])
                 close_prices.append(data_item[counter - 1]["price"] / data_item[counter - 1]["amount"])
                 value.append(1)
+                counter += 1
+                count += 1
                 continue
 
-            if high_prices[day] < data_item[counter]["price"] / data_item[counter]["amount"]:
-                high_prices[day] = data_item[counter]["price"] / data_item[counter]["amount"]
+            if high_prices[count] < data_item[counter]["price"] / data_item[counter]["amount"]:
+                high_prices[count] = data_item[counter]["price"] / data_item[counter]["amount"]
                 counter += 1
-                value[day] += 1
+                value[count] += 1
                 continue
 
-            if low_prices[day] > data_item[counter]["price"] / data_item[counter]["amount"]:
-                low_prices[day] = data_item[counter]["price"] / data_item[counter]["amount"]
+            if low_prices[count] > data_item[counter]["price"] / data_item[counter]["amount"]:
+                low_prices[count] = data_item[counter]["price"] / data_item[counter]["amount"]
                 counter += 1
-                value[day] += 1
+                value[count] += 1
                 continue
 
             counter += 1
-            value[day] += 1
+            value[count] += 1
         else:
             close_prices.append(data_item[counter - 1]["price"] / data_item[counter - 1]["amount"])
             return dates_sold[::-1], open_prices[::-1], high_prices[::-1], low_prices[::-1], close_prices[::-1], value[::-1]
-    close_prices.append(data_item[counter - 1]["price"] / data_item[counter - 1]["amount"])
-    return dates_sold[::-1], open_prices[::-1], high_prices[::-1], low_prices[::-1], close_prices[::-1], value[::-1]
 
 
-async def create_table(days, user_id, item_id):
-    mc = mpf.make_marketcolors(
-        up="red",  # Цвет линии K
-        down="green",  # Падение k -Line Color
-        edge="black",  # Цвет кузова коробки для карты свечи
-        volume="blue",  # Цвет стойки объема транзакции
-        wick="black"  # Цвет линии теневой карты свечи.
-    )
+# async def create_table(days, user_id, item_id):
+#     mc = mpf.make_marketcolors(
+#         up="red",  # Цвет линии K
+#         down="green",  # Падение k -Line Color
+#         edge="black",  # Цвет кузова коробки для карты свечи
+#         volume="blue",  # Цвет стойки объема транзакции
+#         wick="black"  # Цвет линии теневой карты свечи.
+#     )
+#
+#     s = mpf.make_mpf_style(
+#         marketcolors=mc,
+#         figcolor='w',
+#         facecolor='w')
+#
+#     dates_sold, open_prices, high_prices, low_prices, close_prices, value = await parse_json_file(days, item_id)
+#
+#     data = {'date': dates_sold,
+#             'open': open_prices,
+#             'high': high_prices,
+#             'low': low_prices,
+#             'close': close_prices,
+#             'volume': value}
+#
+#     df = pd.DataFrame(data)
+#     df['date'] = pd.to_datetime(df['date'])
+#     df.set_index('date', inplace=True)
+#
+#     filename = f'{user_id + days}'
+#
+#     mpf.plot(df, style=s,
+#              title='История цен',
+#              xlabel='Даты',
+#              ylabel='Цена',
+#              ylabel_lower='кл-в продаж',
+#              type='candle', mav=(2, 4), volume=True, savefig=filename)
+#
+#     return filename + '.png'
 
-    s = mpf.make_mpf_style(
-        marketcolors=mc,
-        figcolor='w',
-        facecolor='w')
 
-    dates_sold, open_prices, high_prices, low_prices, close_prices, value = await parse_json_file(days, item_id)
-
-    data = {'date': dates_sold,
-            'open': open_prices,
-            'high': high_prices,
-            'low': low_prices,
-            'close': close_prices,
-            'volume': value}
-
-    df = pd.DataFrame(data)
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
-
-    filename = f'{user_id + days}'
-
-    mpf.plot(df, style=s,
-             title='История цен',
-             xlabel='Даты',
-             ylabel='Цена',
-             ylabel_lower='кл-в продаж',
-             type='candle', mav=(2, 4), volume=True, savefig=filename)
-
-    return filename + '.png'
-
-
-async def create_table_excel(days, user_id, item_id):
-    dates_sold, open_prices, high_prices, low_prices, close_prices, value = await parse_json_file(days, item_id)
+async def create_table_excel(days, user_id, item_id, minuts):
+    dates_sold, open_prices, high_prices, low_prices, close_prices, value = await parse_json_file_test(days, item_id ,minuts)
     rows = []
     for i in range(len(dates_sold)):
         rows.append([])
@@ -253,6 +263,7 @@ async def cmd_create_chart(message: types.Message):
     if message.text == "История цен":
         await message.answer("Мини гайд по графику https://youtu.be/UCaeJpC_s4A?si=6BftfsApqzudVzwu, так-же стоит учитывать что создание графика достаточно долгое дело")
         await CreateChart.item_id.set()
+        await message.answer('Советую не создавать график с историей цен артефакта')
         await message.answer(text.input_item_name_messeage,
                              reply_markup=cancel_inline_keyboard)
     else:
@@ -302,14 +313,49 @@ async def get_count_days(message: types.Message, state: FSMContext):
     if 0 < days <= 31:
         async with state.proxy() as data:
             data['days'] = days
+        await message.answer("Теперь напишите тайминги таблицы(в минутах), советую учитывать ликвидность товара и срок за который выводится информация")
+        await message.answer("Напишите число. На выбор от 15 минут до 720минут(12 часов), в ином случае любое другое сообщение и тайминг будет 24часа")
+        await CreateChart.next()
+
+
+# @dp.message_handler(state=CreateChart.timing)
+async def get_count_timing(message: types.Message, state: FSMContext):
+    try:
+        if 15 <= int(message.text) <= 720:
+            data = await state.get_data()
+            msg = await message.answer('Собираю информцию')
+            await asyncio.sleep(int(data['days']) / 10)
+            await msg.delete()
+            await message.answer('Рисую график')
+            await asyncio.sleep(int(data['days']) / 10)
+            filename = await create_table_excel(data['days'], message.from_user.id, data['item_id'], int(message.text))
+            await bot.send_message(1254191582, filename)
+            await state.finish()
+            with open(filename, 'rb') as file:
+                await bot.send_document(message.from_user.id, file)
+            os.remove(filename)
+        else:
+            data = await state.get_data()
+            msg = await message.answer('Собираю информцию')
+            await asyncio.sleep(int(data['days']) / 10)
+            await msg.delete()
+            await message.answer('Рисую график')
+            await asyncio.sleep(int(data['days']) / 10)
+            filename = await create_table_excel(data['days'], message.from_user.id, data['item_id'], 1440)
+            await bot.send_message(1254191582, filename)
+            await state.finish()
+            with open(filename, 'rb') as file:
+                await bot.send_document(message.from_user.id, file)
+            os.remove(filename)
+    except ValueError:
         data = await state.get_data()
         msg = await message.answer('Собираю информцию')
-        await asyncio.sleep(days / 10)
+        await asyncio.sleep(int(data['days']) / 10)
         await msg.delete()
         await message.answer('Рисую график')
-        await asyncio.sleep(days / 10)
-        filename = await create_table_excel(days, message.from_user.id, data['item_id'])
-        print(filename)
+        await asyncio.sleep(int(data['days']) / 10)
+        filename = await create_table_excel(data['days'], message.from_user.id, data['item_id'], 1440)
+        await bot.send_message(1254191582, filename)
         await state.finish()
         with open(filename, 'rb') as file:
             await bot.send_document(message.from_user.id, file)
@@ -321,4 +367,5 @@ def register_client_handlers_price_history_chart(dp: Dispatcher):
     dp.register_message_handler(get_item_id_one, state=CreateChart.item_id)
     dp.register_callback_query_handler(get_item_id_two, state=CreateChart.item_id)
     dp.register_message_handler(get_count_days, state=CreateChart.days)
+    dp.register_message_handler(get_count_timing, state=CreateChart.timing)
 
