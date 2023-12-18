fbd240749ba307154e95dba0a217b3080bf426db
Amarsana <fipikmen3@gmail.com>
2023-12-18 21:24:43 +0800

    cnahge: Убрал длинный текст и переписал некоторые элементы кода

--- handlers/client_handler/start.py
+++ handlers/client_handler/start.py
@@ -1,46 +1,31 @@
 from aiogram import types, Dispatcher
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+import text
 from create_bot import bot
 from database.dbsql import reg
 from ..keyboard import main_kb
 
 
-# @dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
-    await bot.send_sticker(message.from_user.id,
-                           r"CAACAgIAAxkBAAEK0RllYhhasr7rJkLg6Rvb6hmlO3GKuAACdxIAAscpaUj6aDiZM6SA4jME")
-    await message.answer("""Бот находится в разработке и на данный момент он бета-тесте""")
+async def cmd_start(message: types.Message):  # cmd = start
     ikb = InlineKeyboardMarkup(row_width=1)
     ikb.add(InlineKeyboardButton('Русский', callback_data='Russian'))
+
+    await bot.send_sticker(message.from_user.id, r"CAACAgIAAxkBAAEK0RllYhhasr7rJkLg6Rvb6hmlO3GKuAACdxIAAscpaUj6aDiZM6SA4jME")
+    await message.answer("""Бот находится в разработке и на данный момент он бета-тесте""")
     await message.answer("Привет! Выбери язык, чтобы окунуться в зимний дух торговли!", reply_markup=ikb)
 
 
-# @dp.callback_query_handler(text=['Russian'])
-async def cmd_server_selection(callback_query: types.CallbackQuery):
-    # language depending on the choice
+async def cmd_server_selection(callback_query: types.CallbackQuery):  # callback = Russian
+    # Выбор языка
     reg(callback_query.from_user.id)
     ikb = InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
     ikb.add(InlineKeyboardButton('RU', callback_data='RU'))
     await bot.send_message(callback_query.from_user.id, "Выберите ваш игровой сервер на этот снегопад❄️:", reply_markup=ikb)
 
 
-# @dp.callback_query_handler(text=['RU'])
-async def cmd_main(callback_query: types.CallbackQuery):
-    await bot.send_message(callback_query.from_user.id, """
-Как-то раз в холодный зимний день, когда природа замерзла в ожидании праздника,я расскажу тебе о чудесном боте, который словно сказочный артефакт из твоих снов, наполненный зимней атмосферой праздника и таинственности.
-
-В этом боте ты найдешь 4 волшебные функции:
-1)❄️Проверка цены: она помогает узнать цену на нужные тебе волшебные предметы.
-2)🎄Ожидание предмета: эта функция позволяет добавить предмет в ожидание по нужным для тебя характеристикам и цене.
-Как только он появится на аукционе, бот превратится в маленького помощника и уведомит тебя о событии,
-главное загадать этот предмет и верить в новогодние чудо.
-3)⛄История цен: создаёт графики цен продаж волшебных предметов,
-как будто таинственные свечи, подаренные самим Дедом Морозом🎅🏻. Ты сможешь выбрать время и изучить каждую свечу и её тень, смотря в неизведанные глубины фитиля.
-4)🍪Средняя цена: покажет тебе среднюю цену продажи предмета.
-
-Этот волшебный бот призван принести в твою жизнь сказочную атмосферу и радость, свойственную зимнему празднику, самое главное не забывать о том что волшебство существует!🍾
-""", reply_markup=main_kb)
+async def cmd_main(callback_query: types.CallbackQuery):  # callback = RU
+    await bot.send_message(callback_query.from_user.id, text.WELCOME_TEXT, reply_markup=main_kb)
 
 
 def register_client_handlers_start(dp: Dispatcher):
