af21f54d6ce5c5aa03af3bfcb175f28994864fc2
Amarsana <fipikmen3@gmail.com>
2023-12-22 17:49:59 +0800

    add: Добавил новую функцию

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -8,7 +8,8 @@ main_button = [
      types.KeyboardButton(text="Удалить запрос")],
     [types.KeyboardButton(text="Средняя цена"),
      types.KeyboardButton(text="История цен")],
-    [types.KeyboardButton(text="Перекуп таблица")]
+    [types.KeyboardButton(text="Перекуп таблица"),
+     types.KeyboardButton(text="Участвовать в розыгрыше")]
 ]
 main_kb = types.ReplyKeyboardMarkup(keyboard=main_button, resize_keyboard=True, one_time_keyboard=False)
 
