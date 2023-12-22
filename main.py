79153ad838ac2a8ca489b37806be5a2693f0a077
Amarsana <fipikmen3@gmail.com>
2023-12-22 17:50:54 +0800

    add: добавил новую команду

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from create_bot import create_bot_factory
 from create_bot import dp
 from database import dbsql
 from handlers.admin_handler import content, report, update
-from handlers.client_handler import feedback, start, get_auc_lot, user_request, average_price, price_history_chart, xlsx_table, reboot_cmd
+from handlers.client_handler import (feedback, start,
+                                     get_auc_lot, user_request,
+                                     average_price, price_history_chart,
+                                     xlsx_table, reboot_cmd,
+                                     registration_in_raffle)
 
 handlers.client_handler.reboot_cmd.register_client_handler_update(dp)
 handlers.client_handler.xlsx_table.register_client_handlers_xlsx_table(dp)
@@ -15,6 +19,7 @@ handlers.client_handler.get_auc_lot.register_client_handlers_get_auc_lot(dp)
 handlers.client_handler.user_request.register_client_handlers_user_request(dp)
 handlers.client_handler.average_price.register_client_handlers_average_price(dp)
 handlers.client_handler.price_history_chart.register_client_handlers_price_history_chart(dp)
+handlers.client_handler.registration_in_raffle.register_client_handlers_registration_in_raffle(dp)
 handlers.admin_handler.report.register_admin_handler_report(dp)
 handlers.admin_handler.content.register_admin_handler_content(dp)
 handlers.admin_handler.update.register_admin_handler_update(dp)
