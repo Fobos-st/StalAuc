63740719bf392ae1b4d4f510287d3323c4840382
Amarsana <fipikmen3@gmail.com>
2024-01-27 17:34:46 +0800

    add and change: Добавил новую систему вывода ошибок, так-же добавил очистку spam_message

--- background_proces.py
+++ background_proces.py
@@ -1,7 +1,9 @@
 import asyncio
 import json
 import math
+import linecache
 import sys
+from datetime import datetime
 
 from aiogram import Bot
 from aiogram.utils.exceptions import ChatNotFound, UserDeactivated, BotBlocked
@@ -14,6 +16,16 @@ from text import notification_text
 from handlers.keyboard import main_kb
 
 bot = Bot(BOT_TOKEN)
+
+
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
 
 
 async def get_lots_item(item_id, user):
@@ -73,6 +85,17 @@ async def checking_conditions(user: tuple, lot: dict) -> bool:
     return param1 and param2 and param3
 
 
+def remaining_time(time_str):
+    time_format = "%Y-%m-%dT%H:%M:%SZ"
+    current_time = datetime.utcnow()
+    target_time = datetime.strptime(time_str, time_format)
+    remaining_time = target_time - current_time
+
+    if remaining_time.days < 0:
+        return True
+    return False
+
+
 async def check_item_rework() -> None:
     spam_message = []  #  Список которых хранит инфу уже отправленных лотов пользователям
     """
@@ -83,62 +106,64 @@ async def check_item_rework() -> None:
     """
     while True:
         print("начало проверки")
-        users = await print_all_users()
+        try:
+            users = await print_all_users()
+            for user in users:
+                    if user[1] == 'None':  # Проверка на наличие зароса у пользователя
+                        continue
+                    result = await get_lots_item(user[1], user)
+                    lots = result['lots']
+                    for lot in lots:
+                        if lot["buyoutPrice"] < user[2]:  # Скип проверки если цена лота больше запроса пользователя
+                            if await checking_conditions(user, lot) and [user[0], lot["endTime"], lot["itemId"]] not in spam_message:
+                                try:
+                                    await bot.send_message(user[0],
+                                                           notification_text.format(dbitem.search_item_name_by_id(user[1]),
+                                                           '{0:,}'.format(lot["buyoutPrice"])),
+                                                           reply_markup=main_kb)
+                                    spam_message.append([user[0], lot["endTime"], lot["itemId"]])
+                                    continue
+                                except ChatNotFound:
+                                    pass
+                                except UserDeactivated:
+                                    pass
+                                except BotBlocked:
+                                    pass
+                        else:
+                            break
 
-        for user in users:
-            try:
-                if user[1] == 'None':  # Проверка на наличие зароса у пользователя
-                    continue
-                result = await get_lots_item(user[1], user)
-                lots = result['lots']
-                for lot in lots:
-                    if lot["buyoutPrice"] < user[2]:  # Скип проверки если цена лота больше запроса пользователя
-                        if await checking_conditions(user, lot) and (user[0], lot["startTime"], lot["itemId"]) not in spam_message:
-                            try:
-                                await bot.send_message(user[0],
-                                                       notification_text.format(dbitem.search_item_name_by_id(user[1]),
-                                                       '{0:,}'.format(lot["buyoutPrice"])),
-                                                       reply_markup=main_kb)
-                                spam_message.append((user[0], lot["startTime"], lot["itemId"]))
-                                continue
-                            except ChatNotFound:
-                                pass
-                            except UserDeactivated:
-                                pass
-                            except BotBlocked:
-                                pass
-                    else:
-                        break
+                    if int(result['total']) > 200:  # KeyError Ошибка с total
+                        iteration = 1
+                        count_iteration = math.ceil(int(result['total']) / 200)
+                        while iteration <= count_iteration:
+                            result = await get_lots_item_more_200(user[1], user, iteration)
+                            lots = result['lots']
+                            for lot in lots:
+                                if lot["buyoutPrice"] < user[2]:
+                                    if await checking_conditions(user, lot) and [user[0], lot["endTime"], lot["itemId"]] not in spam_message:
+                                        try:
+                                            await bot.send_message(user[0],
+                                                                   notification_text.format(dbitem.search_item_name_by_id(user[1]),
+                                                                   '{0:,}'.format(lot["buyoutPrice"])),
+                                                                   reply_markup=main_kb)
+                                            spam_message.append([user[0], lot["endTime"], lot["itemId"]])
+                                            continue
+                                        except ChatNotFound:
+                                            pass
+                                        except UserDeactivated:
+                                            pass
+                                        except BotBlocked:
+                                            pass
+                            iteration += 1
 
-                if int(result['total']) > 200:  # KeyError Ошибка с total
-                    iteration = 1
-                    count_iteration = math.ceil(int(result['total']) / 200)
-                    while iteration <= count_iteration:
-                        result = await get_lots_item_more_200(user[1], user, iteration)
-                        lots = result['lots']
-                        for lot in lots:
-                            if lot["buyoutPrice"] < user[2]:
-                                if await checking_conditions(user, lot) and (user[0], lot["startTime"], lot["itemId"]) not in spam_message:
-                                    try:
-                                        await bot.send_message(user[0],
-                                                               notification_text.format(dbitem.search_item_name_by_id(user[1]),
-                                                               '{0:,}'.format(lot["buyoutPrice"])),
-                                                               reply_markup=main_kb)
-                                        spam_message.append((user[0], lot["startTime"], lot["itemId"]))
-                                        continue
-                                    except ChatNotFound:
-                                        pass
-                                    except UserDeactivated:
-                                        pass
-                                    except BotBlocked:
-                                        pass
-                        iteration += 1
-            except Exception as ex:
-                try:
-                    await bot.send_message(1254191582, f'{ex} \n {lot} \n {user}')
-                except Exception:
-                    await bot.send_message(1254191582, f'{ex} \n {user}')
-        print("Конец проверки")
-        if sys.getsizeof(spam_message) >= 1048576:
-            spam_message = []
-        await asyncio.sleep(45)
+            if sys.getsizeof(spam_message) >= 1048576:
+                spam_message = []
+            for i in range(len(spam_message)):
+                if remaining_time(spam_message[i][1]):
+                    spam_message.pop(i)
+
+            await asyncio.sleep(35)
+
+        except Exception:
+            await bot.send_message(1254191582, PrintException())
+            continue
