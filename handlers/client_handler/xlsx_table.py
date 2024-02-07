756af4396b7777028515d2b8cf58fe3f628178d8
Amarsana <fipikmen3@gmail.com>
2024-02-07 18:38:46 +0800

    change: исправил текст

--- handlers/client_handler/xlsx_table.py
+++ handlers/client_handler/xlsx_table.py
@@ -5,7 +5,7 @@ from create_bot import bot
 
 
 async def send_table(message: types.Message):
-    await message.answer("Таблицы сами за тебя всё подсчитают, ты лишь вводи значения в нужные столбцы и радуйся профиту",
+    await message.answer("Таблица сама за тебя всё подсчитают, ты лишь вводи значения в нужные столбцы и радуйся профиту",
                          reply_markup=handlers.keyboard.main_kb)
     with open("handlers/client_handler/Перекуп Таблица.xlsx", 'rb') as file:  # путь к файлу на линкус
         await bot.send_document(message.from_user.id, file, reply_markup=handlers.keyboard.main_kb)
