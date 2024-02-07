f467f486a13a33f9c19035de041f63635d27ae28
Amarsana <fipikmen3@gmail.com>
2024-02-07 19:10:25 +0800

    add: Добавил рофло текста при вызове аукционна

--- handlers/client_handler/menu.py
+++ handlers/client_handler/menu.py
@@ -4,17 +4,18 @@ from handlers.keyboard import (auction_choice_keyboard,
                                background_process_choice_keyboard_0,
                                background_process_choice_keyboard_1)
 from database.dbsql import check_user_request_lot
-from text import current_request
+from text import current_request, joke_text
+import random
 
 
 async def send_auction_choice(message: types.Message):
-    await message.answer("Text auction", reply_markup=auction_choice_keyboard)
+    await message.answer(joke_text[random.randint(0, 5)], reply_markup=auction_choice_keyboard)
 
 
 async def send_background_process_choice(message: types.Message):
     #  Добавит проверку из бд и выводить инфу об текущем лоте юзера
     if check_user_request_lot(message.from_user.id):
-        await message.answer(text="Text background_process", reply_markup=background_process_choice_keyboard_0)
+        await message.answer(text="У вас нету выбранного предмета", reply_markup=background_process_choice_keyboard_0)
     else:
         await message.answer(text=f"Ваш текущий лот: \n {current_request(message.from_user.id)}",
                              reply_markup=background_process_choice_keyboard_1)
