12b1d6d7af5842932f59beb39b9e8ef00f14d4b3
Amarsana <fipikmen3@gmail.com>
2024-02-17 18:01:43 +0800

    add: функция ошибок

--- text.py
+++ text.py
@@ -1,5 +1,18 @@
 from database.dbsql import get_request_user
 from database.dbitem import search_item_name_by_id, is_it_artifact
+import linecache
+import sys
+
+
+def print_exception():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
 
 text_auc_lot = """
 Количество: {}
